1e4eba16919390258ee7341e3e647229cdd18b95
Tal Karidi-Heller <talkaridiheller@gmail.com>
2022-10-20 16:49:29 +0300

    Finished implementing

--- AbstractClasses/AbstractBasicThreadTask.py
+++ AbstractClasses/AbstractBasicThreadTask.py
@@ -1,5 +1,6 @@
 from typing import *
 from abc import ABC
+import time
 
 from AbstractClasses.AbstractThreadTask import AbstractThreadTask
 from AbstractClasses.AbstractTask import AbstractTask
@@ -7,7 +8,27 @@ from AbstractClasses.AbstractTask import AbstractTask
 
 class AbstractBasicThreadTask(AbstractThreadTask, AbstractTask):
     def get_thread_function(self) -> callable:
-        self.start()
-        while not self.isFinished():
-            self.execute()
-        self.end()
+        def _func():
+            self.start()
+            while not self.isFinished():
+                self.execute()
+            self.end()
+        return _func
+
+
+class SimpleTask(AbstractBasicThreadTask):    
+    def __init__(self, id: int) -> None:
+        self.id = id
+    
+    def start(self) -> None:
+        self.start_time: float = time.time()
+
+    def execute(self) -> None:
+        time.sleep(0.5)
+        print("Another run:", self.id)
+
+    def end(self) -> None:
+        print("finished run:", self.id)
+
+    def isFinished(self) -> None:
+        return time.time() - self.start_time >= 5
--- TaskManager.py
+++ TaskManager.py
@@ -1,7 +1,9 @@
 from concurrent.futures import thread
 from typing import *
 import threading
+from AbstractClasses.AbstractBasicThreadTask import SimpleTask
 from AbstractClasses.AbstractThreadTask import AbstractThreadTask
+import time
 
 
 class TaskManager:
@@ -9,9 +11,16 @@ class TaskManager:
         self.tasks_list: List[AbstractThreadTask] = []
 
     def schedule(self, task: AbstractThreadTask) -> None:
-        task._thread: threading.Thread = threading.Thread(target=task.get_thread_function(task), args=())
+        task._thread: threading.Thread = threading.Thread(target=task.get_thread_function(), args=())
         task._thread.start()
         self.tasks_list.append(task)
 
     def cancel_task(self, task: AbstractThreadTask) -> None:
         task._thread.terminate()
+        self.tasks_list.remove(task)
+
+
+task_manager: TaskManager = TaskManager()
+task_manager.schedule(SimpleTask(1))
+task_manager.schedule(SimpleTask(2))
+time.sleep(10)
